c27445bac75c39c172a3b1d3041af73e70513c6e
Drew Hart <drew@drewhart.com>
2017-02-26 13:28:54 -0600

    added easy and hard levels

--- app.js
+++ app.js
@@ -6,13 +6,36 @@ var correctColor = pickRandomColor();
 var gameHeader = document.querySelector('h1');
 var messageDisplay = document.querySelector('#message');
 var resetButton = document.querySelector('#resetButton');
+var easyButton = document.querySelector('#easyButton')
+var hardButton = document.querySelector('#hardButton')
+
+easyButton.addEventListener('click', whenEasyButtonClicked)
+function whenEasyButtonClicked(){
+  easyButton.classList.add('selected');
+  hardButton.classList.remove('selected');
+  messageDisplay.textContent = '';
+  numOfSquares = 3
+  colors = generateColors(numOfSquares);
+  correctColor = pickRandomColor();
+  for(var i = 0; i < squares.length; i++){
+    if(colors[i]){
+      styleSquare(squares[i], colors[i]);
     } else {
-      messageDisplay.textContent = "Try Again"
-      this.style.background = "#232323";
+      hideSquare(squares[i]);
     }
-  })
+  }
 }
 
+hardButton.addEventListener('click', whenHardButtonClicked)
+function whenHardButtonClicked() {
+  hardButton.classList.add('selected');
+  easyButton.classList.remove('selected');
+  messageDisplay.textContent = '';
+  numOfSquares = 6;
+  colors = generateColors(numOfSquares);
+  correctColor = pickRandomColor();
+  startGame();
+}
 
 function changeColors(color) {
 startGame();
